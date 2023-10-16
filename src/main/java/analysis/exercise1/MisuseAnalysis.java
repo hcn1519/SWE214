537776f769d5ab830efeb39f45ff58f3200d528b
changnam hong <hcn1519@gmail.com>
2023-10-16 15:43:48 -0700

    Exercise 1

--- src/main/java/analysis/exercise1/MisuseAnalysis.java
+++ src/main/java/analysis/exercise1/MisuseAnalysis.java
@@ -3,7 +3,10 @@ package analysis.exercise1;
 import analysis.AbstractAnalysis;
 import analysis.VulnerabilityReporter;
 import soot.Body;
+import soot.RefType;
 import soot.Unit;
+import soot.ValueBox;
+import soot.jimple.internal.JAssignStmt;
 
 public class MisuseAnalysis extends AbstractAnalysis{
 	public MisuseAnalysis(Body body, VulnerabilityReporter reporter) {
@@ -12,6 +15,31 @@ public class MisuseAnalysis extends AbstractAnalysis{
 	
 	@Override
 	protected void flowThrough(Unit unit) {
-		// TODO: Implement your analysis here.
+		Class<? extends Unit> unitClass = unit.getClass();
+		if (!unitClass.equals(soot.jimple.internal.JAssignStmt.class)) {
+			return;
+		}
+		if (!((JAssignStmt) unit).containsInvokeExpr()) {
+			return;
+		}
+		soot.Value rightOp = ((JAssignStmt) unit).getRightOp();
+		if (!this.isCipherRefType(rightOp)) {
+			return;
+		}
+
+		for (ValueBox valueBox : rightOp.getUseBoxes()) {
+			if (this.isVulnerableCipherString(valueBox.getValue().toString())) {
+				reporter.reportVulnerability("<target.exercise1.Misuse: void test()>", unit);
+			}
+		}
+	}
+
+	private boolean isCipherRefType(soot.Value rightOp) {
+		return rightOp.getType() instanceof RefType
+				&& ((RefType) rightOp.getType()).getClassName().equals("javax.crypto.Cipher");
+	}
+
+	private boolean isVulnerableCipherString(String value) {
+		return !value.contains("/");
 	}
 }
