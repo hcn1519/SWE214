b89281fe780b92afea0bce94e47b6d420e539b57
changnam hong <hcn1519@gmail.com>
2023-11-05 00:40:22 -0700

    Exercise2 - CHA Algorithm

--- DECALab2-Soot/src/main/java/analysis/exercise1/CHAAlgorithm.java
+++ DECALab2-Soot/src/main/java/analysis/exercise1/CHAAlgorithm.java
@@ -2,15 +2,13 @@ package analysis.exercise1;
 
 import analysis.CallGraph;
 import analysis.CallGraphAlgorithm;
-import soot.Local;
-import soot.RefType;
-import soot.Scene;
-import soot.SootClass;
-import soot.SootMethod;
-import soot.Unit;
-import soot.jimple.InstanceInvokeExpr;
-import soot.jimple.InterfaceInvokeExpr;
-import soot.jimple.VirtualInvokeExpr;
+import soot.*;
+import soot.jimple.*;
+import soot.jimple.internal.JAssignStmt;
+import soot.jimple.internal.JInvokeStmt;
+import java.util.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CHAAlgorithm extends CallGraphAlgorithm {
 
@@ -24,6 +22,65 @@ public class CHAAlgorithm extends CallGraphAlgorithm {
         // Your implementation goes here, also feel free to add methods as needed
         // To get your entry points we prepared getEntryPoints(scene) in the superclass for you
 
+        soot.Hierarchy hierarchy = scene.getActiveHierarchy();
+        for (SootMethod rootMethod : this.getEntryPoints(scene).collect(Collectors.toList())) {
+            Queue<SootMethod> queue = new ArrayDeque<>();
+            queue.add(rootMethod);
+            cg.addNode(rootMethod);
+
+            while (!queue.isEmpty()) {
+                SootMethod srcMethod = queue.poll();
+                if (!srcMethod.hasActiveBody())
+                    continue;
+
+                Body body = srcMethod.getActiveBody();
+                for (Unit unit : body.getUnits()) {
+                    SootMethod targetMethod = this.targetMethod(unit);
+                    if (targetMethod == null)
+                        continue;
+
+                    if (!cg.hasNode(targetMethod))
+                        cg.addNode(targetMethod);
+
+                    List<SootClass> classes = this.implementingClass(hierarchy, targetMethod);
+                    String targetSignature = targetMethod.getSubSignature();
+                    List<SootMethod> chaMethods = classes.stream()
+                            .map(c -> c.getMethod(targetSignature))
+                            .collect(Collectors.toList());
+
+                    chaMethods.add(targetMethod);
+                    for (SootMethod method : chaMethods) {
+                        if (!cg.hasNode(method))
+                            cg.addNode(method);
+
+                        if (!cg.hasEdge(srcMethod, method)) {
+                            cg.addEdge(srcMethod, method);
+                            queue.add(targetMethod);
+                        }
+                    }
+                }
+            }
+        }
     }
 
+    private SootMethod targetMethod(Unit unit) {
+        if (unit instanceof soot.jimple.internal.JAssignStmt) {
+            JAssignStmt assignStmt = (JAssignStmt) unit;
+            if (assignStmt.containsInvokeExpr())
+                return assignStmt.getInvokeExpr().getMethod();
+
+        } else if (unit instanceof soot.jimple.internal.JInvokeStmt) {
+            JInvokeStmt invokeStmt = (JInvokeStmt) unit;
+            InvokeExpr invokeExpr = invokeStmt.getInvokeExpr();
+            return invokeExpr.getMethod();
+        }
+        return null;
+    }
+
+    private List<SootClass> implementingClass(Hierarchy hierarchy, SootMethod method) {
+        SootClass cls = method.getDeclaringClass();
+        if (cls.isInterface())
+            return hierarchy.getImplementersOf(cls);
+        return new ArrayList<>(Collections.singleton(cls));
+    }
 }
